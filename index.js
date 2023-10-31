e363f67db66734fc4e25917b0002ff28c6dbd6b5
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-10-31 14:02:40 -0500

    added flavorObject to store individual flavor and cout

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //make function named userFlavorCount
 //that accepts a string of a list of flavors as a parameter named flavorList
 const userFlavorCount =(flavorList) => {
-    console.log(flavorList);
+    const flavorsArray = flavorList.split(',');
+    let flavorObject = {};
+    flavorObject[flavorsArray[0]] = 1;
+    console.log(flavorObject);
 }
-userFlavorCount('vanilla,chocolate,peach,strawberry');
+//mak const variable named flavorsArray that takes flavorList string and splits into array
+//make const variable named flavorObject set empty
+userFlavorCount('vanilla,chocolate,peach,chocoalate,vanilla,vanilla,strawberry');
