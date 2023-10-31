14676ab5ea3fbc05bafa49c7f08692331b482c72
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-10-31 13:49:34 -0500

    added function userFlavorCount and tested

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('test');
+//make function named userFlavorCount
+//that accepts a string of a list of flavors as a parameter named flavorList
+const userFlavorCount =(flavorList) => {
+    console.log(flavorList);
+}
+userFlavorCount('vanilla,chocolate,peach,strawberry');
