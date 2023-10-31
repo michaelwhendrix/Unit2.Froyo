a97878a3e87f167973fcf19b1aadee9a95901281
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-10-31 14:49:02 -0500

    added variable for array of keys and loops and if to check if flavor is in object and increment count

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const userFlavorCount =(flavorList) => {
     const flavorsArray = flavorList.split(',');
     let flavorsObject = {};
     for(let i = 0; i < flavorsArray.length; i++) {
-        flavorsObject[flavorsArray[i]] = 1;
+        flavorsObject[flavorsArray[i]] = 0;
+        }
+        const flavorKeys = Object.keys(flavorsObject);
+        console.log(flavorKeys);
+    for(let i = 0; i < flavorsArray.length; i++) {
+        for(let j = 0; j < flavorKeys.length; j++) {
+            if(flavorsArray[i] === flavorKeys[j]) {
+                flavorsObject[flavorKeys[j]]++;
+            }
+        }
     }
     console.log(flavorsObject);
 }
+
 //mak const variable named flavorsArray that takes flavorList string and splits into array
-//make const variable named flavorObject set empty
-userFlavorCount('vanilla,chocolate,peach,chocoalate,vanilla,vanilla,strawberry');
+//make const variable named flavorObject set empty object
+// add for loop to get each index in array
+userFlavorCount('vanilla,chocolate,peach,chocolate,vanilla,vanilla,strawberry');
